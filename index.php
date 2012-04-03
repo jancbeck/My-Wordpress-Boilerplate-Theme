a9d2cdeea1fb70895a558e8b5ad9d2c40546a203
4cJan <jb@cccc.de>
2012-04-03 13:46:09 +0200

    new IE conditional classes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
-<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7 no-js" lang="de-DE"> <![endif]-->
-<!--[if IE 7]><html class="lt-ie9 lt-ie8 no-js" lang="ende-DE> <![endif]-->
-<!--[if IE 8]><html class="lt-ie9 no-js" lang="de-DE"> <![endif]-->
+<!--[if lt IE 7]> <html class="lte-ie9 lte-ie8 lte-ie7 lte-ie6 ie6 no-js" lang="de-DE"> <![endif]-->
+<!--[if IE 7]><html class="lte-ie9 lte-ie8 lte-ie7 ie7 no-js" lang="ende-DE> <![endif]-->
+<!--[if IE 8]><html class="lte-ie9 lte-ie8 ie8 no-js" lang="de-DE"> <![endif]-->
 <!--[if IE 9]><html class="ie9 no-js" lang="de-DE"> <![endif]-->
 <!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="de-DE"> <!--<![endif]-->
 <head>
