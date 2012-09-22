3834ec3dc3ae2db6a4f4de2b084bcfe0aa4fe803
jancbeck <delite.jcb@gmail.com>
2012-09-23 00:47:13 +0200

    header part now in php
    past showed that i do this in every project and it became anoying

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
-<!doctype html>
-<!--[if lt IE 7]> <html class="lte-ie9 lte-ie8 lte-ie7 lte-ie6 ie6 no-js" lang="de"> <![endif]-->
-<!--[if IE 7]><html class="lte-ie9 lte-ie8 lte-ie7 ie7 no-js" lang="de"> <![endif]-->
-<!--[if IE 8]><html class="lte-ie9 lte-ie8 ie8 no-js" lang="de"> <![endif]-->
-<!--[if IE 9]><html class="ie9 no-js" lang="de"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="de"> <!--<![endif]-->
-<head>
-	<meta charset="utf-8" />
-	
-	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement) /* removes no-js class */</script>
-	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
-	<meta name="generator" content="WordPress" />
-	
-	<link rel="icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" type="image/x-icon" />
-	
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-
 <h1>It works.</h1>
 
 <?php get_footer(); ?>
-
-</body>
-</html>
