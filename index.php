79fff3608fda70d8b24434e50ba08137e486a23b
Jan <m@jancb.de>
2013-03-22 16:30:29 +0100

    concatinated dummy content

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php part( 'header' ) ?>
 
-<?php foreach ( array( 'navigation', 'content', 'lists', 'forms' ) as $name ) : ?>
-	
-	<h2><?php echo ucfirst( $name )?></h2>
-	
-	<?php part( 'dummy', $name ) ?>
-
-<?php endforeach ?>
+	<?php part( 'dummy' ) ?>
 
 <?php part( 'footer' ) ?>
---
+++ parts/dummy.php
@@ -0,0 +1,352 @@
+<h3>Breadcrumb</h3>
+
+<nav class="breadcrumb">
+	<a href="#">This is a link</a> /
+	<a href="#">Lorem link</a> / 
+	<a href="#">Dolor link two</a> /
+	<a href="#">Here is a link</a> /
+	<strong>Active Link</strong>
+</nav>			
+		
+
+<h3>Horizontal Buttons</h3>
+
+<nav role="navigation">
+	<ul>
+		<li><a href="#">This is a link</a></li>
+		<li class="active"><a href="#">Active Button</a></li>
+		<li><a href="#">Lorem link</a></li>
+		<li><a href="#">Dolor link two</a></li>
+		<li><a href="#">Here is a link</a></li>
+	</ul>
+</nav>
+
+
+<h3>Horizontal Links</h3>
+
+<nav role="navigation">
+	<ul>
+		<li><a href="#">This is a link</a></li>
+		<li class="active"><a href="#">Active Link</a></li>
+		<li><a href="#">Lorem link</a></li>
+		<li><a href="#">Dolor link two</a></li>
+		<li><a href="#">Here is a link</a></li>
+	</ul>
+</nav>			
+
+
+<h3>Tabbed Navigation</h3>
+
+<nav role="navigation">
+	<ul>
+		<li><a href="#">This is a link</a></li>
+		<li class="active"><a href="#">Active Link</a></li>
+		<li><a href="#">Lorem link</a></li>
+		<li><a href="#">Dolor link two</a></li>
+		<li><a href="#">Here is a link</a></li>
+	</ul>
+</nav>			
+
+
+<h3>Pagination</h3>
+
+<nav class="page">
+	<ul>
+		<li class="page-prev"><a href="#">← Prev</a></li>
+		<li><a href="#">1</a></li>
+		<li><a href="#">2</a></li>
+		<li><a href="#">3</a></li>
+		<li class="page-next"><a href="#">Next →</a></li>
+	</ul>
+</nav>
+
+
+<h3>Vertical Links</h3>
+
+<nav role="navigation">
+	<ul>
+		<li><a href="#">This is a link</a></li>
+		<li class="active"><a href="#">Active Link</a></li>
+		<li><a href="#">Lorem link</a></li>
+		<li><a href="#">Dolor link two</a></li>
+		<li><a href="#">Here is a link</a></li>
+	</ul>
+</nav>			
+
+
+<h3>Article</h3>
+
+<article>
+	<header>
+		<h2>The Title Goes Here</h2>	
+	</header>
+	
+	<aside>
+		This is aside text lorem ipsum dolor sit amet consectetur adipisicing.
+	</aside>
+	
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <a href="#">nostrud exercitation</a> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod <a href="#">tempor incididunt</a> ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
+	<footer>
+		Posted August 22, 2011
+	</footer>
+</article>
+
+
+<h3>Data Table</h3>
+
+<table>
+	<thead>
+		<tr>
+			<th>Type</th>
+			<th>Date</th>
+			<th>Rating</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>Cheddar</td>
+			<td>Jan 3, 2012</td>
+			<td>★★★</td>
+		</tr>
+		<tr>
+			<td>Havarti</td>
+			<td>Jan 12, 2012</td>
+			<td>★★★★</td>
+		</tr>
+		<tr>
+			<td>Muenster</td>
+			<td>Jan 20, 2012</td>
+			<td>★★</td>
+		</tr>
+		<tr>
+			<td>Swiss</td>
+			<td>Jan 22, 2012</td>
+			<td>★</td>
+		</tr>
+		<tr>
+			<td>Gouda</td>
+			<td>Jan 25, 2012</td>
+			<td>★★★★★</td>
+		</tr>
+		<tr>
+			<td>Emmentaler</td>
+			<td>Jan 27, 2012</td>
+			<td>★★★</td>
+		</tr>
+	</tbody>
+</table>			
+
+
+<h3>Footer</h3>
+
+<footer role="contentinfo">
+	<nav role="navigation">
+		<ul>
+			<li><a href="#">This is a link</a></li>
+			<li><a href="#">Active Link</a></li>
+			<li><a href="#">Lorem link</a></li>
+			<li><a href="#">Dolor link two</a></li>
+			<li><a href="#">Here is a link</a></li>
+		</ul>
+	</nav>		
+	<p>Copyright © 2012 Company Co. All rights reserved.</p>
+</footer>			
+				
+
+<h3>Slats</h3>
+
+<ol class="slats">
+	<li class="group">
+		<a href="#">
+			<img src="http://placehold.it/66x66&text=X" alt="thumbnail" />
+			<h3>This is the title</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. <span class="meta">August 10, 2011</span></p>
+		</a>
+	</li>
+	<li class="group">
+		<a href="#">
+			<img src="http://placehold.it/66x66&text=X" alt="thumbnail" />
+			<h3>This is the title</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. <span class="meta">August 10, 2011</span></p>
+		</a>
+	</li>
+	<li class="group">
+		<a href="#">
+			<img src="http://placehold.it/66x66&text=X" alt="thumbnail" />
+			<h3>This is the title</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. <span class="meta">August 10, 2011</span></p>
+		</a>
+	</li>
+</ol>		
+
+<h3>Thumbnail Grids</h3>	
+
+<ol class="thumb-grid group">
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+	<li><a href="#"><img src="http://placehold.it/160x160&text=X" alt="thumbnail" /></a></li>
+</ol>			
+
+<h3>Definition List</h3>
+
+<dl>
+	<dt>The title is here</dt>
+	<dd>Lorem ipsum definition goes here dolor sit amet.</dd>
+	<dt>The title is here</dt>
+	<dd>Lorem ipsum definition goes here dolor sit amet.</dd>
+	<dt>The title is here</dt>
+	<dd>Lorem ipsum definition goes here dolor sit amet.</dd>
+</dl>					
+				
+<h3>Meta</h3>
+
+<ol class="tags">
+	<li><a href="#"><span class="meta">123</span> cheddar</a></li>
+	<li><a href="#"><span class="meta">34</span> swiss</a></li>
+	<li><a href="#"><span class="meta">1,234</span> havarti</a></li>
+	<li><a href="#"><span class="meta">8</span> gouda</a></li>
+	<li><a href="#"><span class="meta">56</span> manchego</a></li>
+</ol>	
+
+<h3>Multi - labels left</h3>
+
+<form action="/">
+	<fieldset>
+		<label for="name">Name</label>
+		<input type="text" id="name" class="form-text" />
+		<p class="form-help">This is help text under the form field.</p>
+	</fieldset>
+	
+	<fieldset>
+		<label for="email">Email</label>
+		<input type="email" id="email" class="form-text" />
+	</fieldset>
+	
+	<fieldset>
+		<label for="gender">Gender</label>
+		<select id="gender">
+			<option>Male</option>
+			<option>Female</option>
+			<option>Cylon</option>
+		</select>
+	</fieldset>
+
+	<fieldset class="radio">
+		<label for="notifications">Notifications</label>
+		<ul>
+			<li><label><input type="radio" name="notifications" /> Send me email</label></li>
+			<li><label><input type="radio" name="notifications" /> Don't send me email</label></li>
+			<li><label><input type="radio" name="notifications" /> Send me flowers</label></li>
+		</ul>
+	</fieldset>
+
+	<fieldset>
+		<label for="url">URL</label>
+		<input type="url" id="url" class="form-text" placeholder="http://yourdomain.com" />
+	</fieldset>
+
+	<fieldset>
+		<label for="bio">Bio</label>
+		<textarea id="bio"></textarea>
+	</fieldset>
+	
+	<fieldset class="check">
+		<label><input type="checkbox" /> I accept the terms of service and lorem ipsum.</label>
+	</fieldset>
+
+	<fieldset class="form-actions">
+		<input type="submit" value="Submit" />
+	</fieldset>
+</form>			
+			
+<h3>Multi - labels top</h3>
+
+<form action="/">
+	<fieldset>
+		<label for="name">Name</label>
+		<input type="text" id="name" class="form-text" />
+		<p class="form-help">This is help text under the form field.</p>
+	</fieldset>
+	
+	<fieldset>
+		<label for="email">Email</label>
+		<input type="email" id="email" class="form-text" />
+	</fieldset>
+	
+	<fieldset>
+		<label for="gender">Gender</label>
+		<select id="gender">
+			<option>Male</option>
+			<option>Female</option>
+			<option>Cylon</option>
+		</select>
+	</fieldset>
+
+	<fieldset class="radio">
+		<label for="notifications">Notifications</label>
+		<ul>
+			<li><label><input type="radio" name="notifications" /> Send me email</label></li>
+			<li><label><input type="radio" name="notifications" /> Don't send me email</label></li>
+			<li><label><input type="radio" name="notifications" /> Send me flowers</label></li>
+		</ul>
+	</fieldset>
+
+	<fieldset>
+		<label for="url">URL</label>
+		<input type="url" id="url" class="form-text" placeholder="http://yourdomain.com" />
+	</fieldset>
+
+	<fieldset>
+		<label for="bio">Bio</label>
+		<textarea id="bio"></textarea>
+	</fieldset>
+	
+	<fieldset class="check">
+		<label><input type="checkbox" /> I accept the terms of service and lorem ipsum.</label>
+	</fieldset>
+
+	<fieldset class="form-actions">
+		<input type="submit" value="Submit" />
+	</fieldset>
+</form>			
+				
+
+<h3>Search</h3>
+
+<form action="/" class="search">
+	<fieldset>
+		<input type="text" placeholder="Search" />
+		<input type="submit" value="Go" />
+	</fieldset>
+</form>			
+				
+<h3>Simple - top labels</h3>
+
+<form action="/">
+	<fieldset>
+		<label for="name">Name</label>
+		<input type="text" id="name" class="form-text" />
+		<p class="form-help">This is help text under the form field.</p>
+	</fieldset>
+	
+	<fieldset>
+		<label for="email">Email</label>
+		<input type="email" id="email" class="form-text" />
+	</fieldset>
+
+	<fieldset class="form-actions">
+		<input type="submit" value="Submit" />
+	</fieldset>
+</form>
+
+					
