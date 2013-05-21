fd2216ca922b4b76253145d5b139d1c70bf5e42d
Jan <m@jancb.de>
2013-05-21 19:44:33 +0200

    added wp language function

--- parts/header.php
+++ parts/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]> <html class="lte-ie9 lte-ie8 lte-ie7 lte-ie6 ie6 no-js" lang="de"> <![endif]-->
-<!--[if IE 7]><html class="lte-ie9 lte-ie8 lte-ie7 ie7 no-js" lang="de"> <![endif]-->
-<!--[if IE 8]><html class="lte-ie9 lte-ie8 ie8 no-js" lang="de"> <![endif]-->
-<!--[if IE 9]><html class="ie9 no-js" lang="de"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="de"> <!--<![endif]-->
+<!--[if lt IE 7]> <html class="lte-ie9 lte-ie8 lte-ie7 lte-ie6 ie6 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]><html class="lte-ie9 lte-ie8 lte-ie7 ie7 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]><html class="lte-ie9 lte-ie8 ie8 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 9]><html class="ie9 no-js" <?php language_attributes(); ?>> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
 	<meta charset="utf-8" />
 	
@@ -14,6 +14,3 @@
 </head>
 
 <body <?php body_class() ?>>
-
-<h1><?php bloginfo( 'name' ) ?></h1>
-<?php part( 'navigation' ) ?>
