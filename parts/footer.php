307a0eae49970ac24591a3bc7045b9c0173c70ee
Jan <m@jancb.de>
2013-05-26 14:33:09 +0200

    removed footer markup

--- parts/footer.php
+++ parts/footer.php
@@ -1,16 +1,3 @@
-<footer role="contentinfo">
-	<nav role="navigation">
-		<ul>
-			<li><a href="#">This is a link</a></li>
-			<li><a href="#">Active Link</a></li>
-			<li><a href="#">Lorem link</a></li>
-			<li><a href="#">Dolor link two</a></li>
-			<li><a href="#">Here is a link</a></li>
-		</ul>
-	</nav>		
-	<p>Copyright © 2012 Company Co. All rights reserved.</p>
-</footer>	
-
 <?php wp_footer() ?>
 
 </body>
